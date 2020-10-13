1cf730e0799a50f07b91704debf160bc695e0d25
FAuditore <951165464@qq.com>
2020-10-13 17:47:00 +0800

    &

--- channel.go
+++ channel.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	/*
@@ -13,46 +16,67 @@ func main() {
 		发送数据
 			[chanName] <- [value]
 		一次发送一次阻塞
+
+
+		缓冲通道 通道满了阻塞
+		make(chan type,capacity)
+
+		双向通道
+			chan <- data 发送数据 写出
+			data <- chan 获取数据 读取
+		单向通道
+			chan <- T 只能写
+			<- chan T 只能读
 	*/
 	var a chan int
 	a = make(chan int)
 
 	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println("zi goroutine", i)
-		}
 		a <- 1
 	}()
 	data := <-a
 	fmt.Println(data)
 
+	ch1:=make(chan int,5)
+	ch1 <- 100
+	fmt.Println(len(ch1),cap(ch1)) // 1,5
+
+
+
 	ch2 := make(chan int)
-	go senddata(ch2)
-	for {
-		v, ok := <-ch2
-		if ok {
-			fmt.Println(v, ok)
-		} else {	//channel close之后，读取会返回0值和false
-			break
-		}
-	}
+	go sendData(ch2)
 
-	ch3 := make(chan int)
-	go senddata2(ch3)
-
-	for v := range ch3 {
+	for v := range ch2 {
 		fmt.Println(v)
 	}
-}
 
-func senddata(c chan int) {
-	for i := 0; i < 10; i++ {
-		c <- i
+
+	var ch = make(chan int)
+	//只写channel
+	go func(ch chan<- int) {
+		ch <- 1
+		ch <- 2
+		ch <- 3
+		close(ch)
+	}(ch)
+forLoop:
+	for {
+		select {
+		case i, ok := <-ch:
+			if ok {
+				fmt.Println("receive ", i)
+			}else {
+				fmt.Println("channel closed")
+				break forLoop
+			}
+		case <-time.After(time.Second):
+			fmt.Println("Time out")
+			break forLoop
+		}
 	}
-	close(c)
 }
 
-func senddata2(c chan int) {
+func sendData(c chan int) {
 	for i := 0; i < 10; i++ {
 		c <- i * 2
 	}
